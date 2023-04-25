a6a4f01f1f98d7c070709a416fa90b34a722491d
Henriquedbrito <rm98831@fiap.com.br>
2023-04-25 11:50:24 -0300

    adicionando banner

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -47,25 +47,64 @@
 
 
 
-function soma(p1,p2,p3) {
-    nr1 = p1;
-    nr2 = p2;
+// function soma(p1,p2,p3) {
+//     nr1 = p1;
+//     nr2 = p2;
 
-    return (parseInt(nr1) + parseInt(nr2) + parseInt(p3))
+//     return (parseInt(nr1) + parseInt(nr2) + parseInt(p3))
 
-    // console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`)
-    // console.log(`Este é o parametro que foi passado : ${parametro}`);
-}
+//     // console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`)
+//     // console.log(`Este é o parametro que foi passado : ${parametro}`);
+// }
 
 
-//ARROW FUNCTION
-const mudaCor = ()=>{
+// //ARROW FUNCTION
+// const mudaCor = ()=>{
 
-    //FUNÇÃO SET-TIME-OUT
-    setTimeout(alert("EXECUTOU"),5000);
+//     //FUNÇÃO SET-TIME-OUT
+//     setTimeout(alert("EXECUTOU"),5000);
+
+// }
+
+// mudaCor();
+
+
+
+
+function mudaCor() {
+
+    let r = "";
+    let g = "";
+    let b = "";
+
+    //random = Gera números aleatórios entre 0 e 1
+    //ceil,floor e round que arredondam o úmero para cima, para baixo ou aleatóriamente.
+    //Determinando para a variavel um valor entre 0 e 250
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+
+    const cabecalho = document.querySelector(".cabecalho");
+
+
+
+    cabecalho.setAttribute("style", `background-color:rgb(${r},${g},${b})`);
+
+    tmp = setTimeout(mudaCor, 1000)
 
 }
 
 mudaCor();
 
+function alteraBanner() {
 
+    let nr = Math.round(Math.random() *3)
+
+    let caminho = `./img/banner-lateral-${nr}.png`;
+
+    const img3 = document.querySelector(".l-e > img")
+    img3.src = caminho;
+
+}
+
+alteraBanner();
